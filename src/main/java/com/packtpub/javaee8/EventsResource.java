8d24a9c40a67c9e13f984424ee919eb0f4dbd0b2
Mario-Leander Reimer <mario-leander.reimer@qaware.de>
2018-03-20 09:08:27 +0100

    Prepared video 5.2 template.

--- src/main/java/com/packtpub/javaee8/EventsResource.java
+++ src/main/java/com/packtpub/javaee8/EventsResource.java
@@ -15,50 +15,12 @@ import java.time.LocalDateTime;
 @Path("events")
 public class EventsResource {
 
-    private volatile SseEventSink eventSink;
+    // TODO implement open event stream
 
-    @GET
-    @Produces(MediaType.SERVER_SENT_EVENTS)
-    public void openEventStream(@Context final SseEventSink eventSink) {
-        this.eventSink = eventSink;
-    }
+    // TODO implement send events
 
-    @POST
-    public void sendEvent(String message, @Context Sse sse) {
-        final SseEventSink localSink = eventSink;
-        if (localSink == null) return;
+    // TODO implement close event stream
 
-        // send simple event
-        OutboundSseEvent event = sse.newEvent(message);
-        localSink.send(event);
-
-        // send simple string event
-        OutboundSseEvent stringEvent = sse.newEvent("stringEvent", message + " From server.");
-        localSink.send(stringEvent);
-
-        // send primitive long event using builder
-        OutboundSseEvent primitiveEvent = sse.newEventBuilder()
-                .name("primitiveEvent")
-                .data(System.currentTimeMillis()).build();
-        localSink.send(primitiveEvent);
-
-        // send JSON-B marshalling to send event
-        OutboundSseEvent jsonbEvent = sse.newEventBuilder()
-                .name("jsonbEvent")
-                .data(new JsonbSseEvent(message))
-                .mediaType(MediaType.APPLICATION_JSON_TYPE)
-                .build();
-        localSink.send(jsonbEvent);
-    }
-
-    @DELETE
-    public void closeEventStream() throws IOException {
-        final SseEventSink localSink = eventSink;
-        if (localSink != null) {
-            this.eventSink.close();
-        }
-        this.eventSink = null;
-    }
 
     @JsonbPropertyOrder({"time", "message"})
     public static class JsonbSseEvent {
